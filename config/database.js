b526e63429ac226a52f45487dac6bd0d4511736b
Guru2402 <prasadguru141@gmail.com>
2018-01-15 12:13:10 +0530

    done changes connected db

---
+++ config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'connection': {
+        'host': 'us-cdbr-iron-east-05.cleardb.net',
+        'user': 'b0b241683def69',
+        'password': '6d231ab0'
+    },
+	'database': 'heroku_bb15082ba0347b3?reconnect=true',
+    'users_table': 'users'
+};
